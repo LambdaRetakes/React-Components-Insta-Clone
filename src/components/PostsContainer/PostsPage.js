a38f4258bd0778d440a2d931d356405b022cd480
Khari <kharris2k16@gmail.com>
2020-06-10 08:10:28 -0700

    mvp

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,17 +3,18 @@
 import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+// import dummyData here on the parent component
 import dummyData from '../../dummy-data';
 
 
 const PostsPage = () => {
-  const [data, setData] = useState(dummyData);
+  const [data, setData] = useState(dummyData); //set dummyData to our state
   // set up state for your data
   return (
     <div className="posts-container-wrapper">
-      {data.map((post, index) => {
-        return <Post post={post} key={index} />
+    {/* render a Post component for item in our dummy data as a post, and set the post props object to the individual post */}
+      {data.map((post) => {
+        return <Post post={post} />
       })}
     </div>
   );
